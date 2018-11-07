47e0de371a6e985a64f523a9b997bd92237770c5
SCUR0 <joelm04@gmail.com>
2018-11-07 12:17:19 -0800

    Update Get-MWOLeaderboard.ps1
    Optimization pass. Code simplified, added parameters.

--- Get-MWOLeaderboard.ps1
+++ Get-MWOLeaderboard.ps1
@@ -8,6 +8,12 @@
     should be expected to take a long time as it has to go parse multiple
     pages.
 
+.PARAMETER mwoEmail
+MWO email used to login mwo.
+
+.PARAMETER password
+Used if you want to automate login. Can be left blank if you prefer manual input.
+
 .PARAMETER global
 Only pulls global data instead of all classes.
 
@@ -23,6 +29,9 @@ The location you want to save. Script will dynamically default to documents fold
 param (
     [switch]$global,
     [Parameter(Mandatory=$True)]
+    [string]$mwoEmail,
+    $password,
+    [Parameter(Mandatory=$True)]
     [string]$season,
     $savepath = [Environment]::GetFolderPath("MyDocuments")
 )
@@ -31,53 +40,38 @@ param (
 function ParseTable {
     param(
         [Parameter(Mandatory = $true)]
-        [Microsoft.PowerShell.Commands.HtmlWebResponseObject] $WebRequest
-    )
+        [Microsoft.PowerShell.Commands.HtmlWebResponseObject] $WebRequest,
+        [switch]$FirstParse,
+        [Parameter(Mandatory = $true)]
+        [System.IO.StreamWriter]$TextStream
 
-    #Extract the tables out of the web request
-    try {
-        $tables = @($WebRequest.ParsedHtml.getElementsByTagName("table"))
-    }
-    catch{
+    )
+    
+    $table = @($WebRequest.ParsedHtml.getElementsByTagName("table"))
+    if (!$table){
         Write-Error "An Error was encountered while trying to pull element by tag `
         name table."
     }
-    $table = $tables[0]
-    $titles = @()
-    $rows = @($table.Rows)
-
-    #Go through all of the rows in the table
-    foreach($row in $rows){
-        $cells = @($row.Cells)
-        if($cells[0].tagName -eq "th"){
-            $titles = @(
-                foreach($cell in $cells){
-                    ("" + $cell.InnerText).Trim() 
+    $rownum = 0
+    #Get all the rows in the tables
+    ForEach($row in $table.rows){
+        #Treat the first row as a header
+        if($rownum -eq 0){
+            if ($FirstParse){
+                $RowArray = @()
+                ForEach($cell in $row.cells){
+                    $RowArray += "`"$(($cell.innerHTML))`""
                 }
-            )
-            continue
+                $TextStream.WriteLine($RowArray -join "`t")
+            }           
+        }else{
+            $RowArray = @()
+            ForEach($cell in $row.cells){
+                $RowArray += "`"$(($cell.innerHTML))`""
+            }
+            $TextStream.WriteLine($RowArray -join "`t")
         }
-
-        #create titles if not found
-        if(-not $titles){
-            $titles = @(
-                foreach ($cell in (1..($cells.Count + 2))){
-                     "P$cell" 
-                }
-            )
-        }
-
-        <#Now go through the cells in the the row. For each, try to find the
-        title that represents that column and create a hash#>
-        $resultObject = [Ordered] @{}
-        for($counter = 0; $counter -lt $cells.Count; $counter++){
-            $title = $titles[$counter]
-            if(-not $title) { continue }
-            $resultObject[$title] = ("" + $cells[$counter].InnerHTML
-        }
-
-        #hashtable to PSCustomObject
-        [PSCustomObject] $resultObject
+        $rownum++
     }
 }
 
@@ -95,14 +89,18 @@ try{
 #Set SSL to updated version
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+
+if (!$password){
+    #request password if left out of parameter
+    $passwordString = read-host -AsSecureString "Password?"
+    $password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($PasswordString))
+}
+
 #Configuration varriables
-$username = Read-Host "MWO Username (email)?"
-$passwordString = read-host -AsSecureString "Password?"
 #Respect Original Progress Preference
 $OriginalProgressPreference=$ProgressPreference
 #subtract 1 due to MWO leaderboard format for season
 $seasonquery = $season - 1
-$Password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($PasswordString))
 $loginUrl = "https://mwomercs.com/profile/leaderboards"
 $ErrorCount = 0
 $leaderboards =@{
@@ -142,7 +140,7 @@ Foreach ($Class in $ClassArray){
 
 $r = Invoke-WebRequest -Uri('https://mwomercs.com/do/login') -SessionVariable mwo
 $form = $r.Forms
-$form.fields['email'] = $username
+$form.fields['email'] = $mwoEmail
 $form.fields['password'] = $password
 
 #set cookies
@@ -160,9 +158,9 @@ $mwo.Cookies.Add($seasoncookie);
 
 
 #Submit loginform
-$r=Invoke-WebRequest -Uri ('https://mwomercs.com/do/login') -WebSession $mwo -Method POST -Body $form.Fields
-Write-Host "Parse request initialized." 
-start-sleep 3
+Invoke-WebRequest -Uri ('https://mwomercs.com/do/login') -WebSession $mwo -Method POST -Body $form.Fields | Out-Null
+Write-Verbose "Parse request initialized." -Verbose
+start-sleep 2
 
 #pull leaderboards 
 
@@ -170,7 +168,9 @@ $Leaderboards.GetEnumerator() | Invoke-Parallel -ImportVariables -ImportFunction
     $page= 0
     $rawtables = @()
     $leaderboardpage = $null
-    write-host "Parsing $($_.name)..."
+    $FirstParse = $true
+    $TextStream=[System.IO.StreamWriter]"$savepath\$($_.name +"_"+ $season).csv"
+    Write-Verbose "Parsing $($_.name)..." -Verbose
     while ($leaderboardpage.Content -notlike "*No results found.*"){
         do{
             $ParseFail = $null
@@ -179,28 +179,32 @@ $Leaderboards.GetEnumerator() | Invoke-Parallel -ImportVariables -ImportFunction
             $ProgressPreference = $OriginalProgressPreference
             if ($leaderboardpage.Content -notlike "*No results found.*"){
                 Try {
-                        ParseTable $leaderboardpage -ErrorAction Stop | `
-                        Export-Csv "$savepath\$($_.name +"_"+ $season).csv" -Delimiter "`t" -NoTypeInformation -Append
+                        if ($FirstParse){
+                            ParseTable -WebRequest $leaderboardpage -FirstParse -TextStream $TextStream -ErrorAction Stop
+                        }else{
+                            ParseTable -WebRequest $leaderboardpage -TextStream $TextStream -ErrorAction Stop
+                        }
                     }
                 catch {
-                    Write-Warning "Error encountered during parse. Retrying..."
+                    Write-Warning "Error encountered during parse of $($_.name). Retrying..."
                     $ParseFail = $true
                     $ErrorCount++
                     if ($ErrorCount -ge 1){
                         Write-Warning "Retry number $ErrorCount."
                     }
                     Start-Sleep -Seconds 30
-                    }
+                }
             }
         }until ((!$ParseFail) -or ($ErrorCount -ge 5))
         if (($ParseFail) -and ($ErrorCount -ge 5)){
-                Write-Error "Parse error retry exceeded"
-                pause
-                exit
-            }
+            $TextStream.close()    
+            Write-Error "Parse error retry exceeded"
+            pause
+            exit
+        }
+        $FirstParse=$false
         $page++
-        #Write-Progress -Activity "Scanning $($leaderboard.name) Pages..." -Status "Page: $page"
     }
-    #Write-Progress -Activity "Scanning $($leaderboard.name) Pages..." -Completed
-    Write-Output "$($_.name) saved to $savepath\$($_.name +"_"+ $season).csv"  
+    $TextStream.close()
+    Write-Verbose "$($_.name) saved to $savepath\$($_.name +"_"+ $season).csv" -Verbose
 }
